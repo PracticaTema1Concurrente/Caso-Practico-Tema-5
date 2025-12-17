c81e0f2c4498b53dbcf6aaedc8804e75c2780964
gabriel140282 <gkaakmor@myuax.com>
2025-12-17 11:35:15 +0100

    V0.1

---
+++ src/test/java/com/starkindustries/casopracticotema5/CasoPracticoTema5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starkindustries.casopracticotema5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasoPracticoTema5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
